dfb1a28dc872a94d632d65e67d03cb9ff4587625
Marilia <ramonap2013@gmail.com>
2023-11-24 17:42:35 -0600

    pinball map lat & lon

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('https://pinballmap.com/api/v1/locations/closest_by_lat_lon.json', async (req, res) => {
+    const { lat, lon } = req.query;
+
+    try {
+        const apiRespone = await axiost.get(`https://pinballmap.com/api/v1/locations/closest_by_lat_lon.json?lat=${lat}&{lon}'`);
+        const locations = apiResponse.data;
+
+        res.json(locations);
+    } catch (error) {
+        console.error('Error fetching pinball locations:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import Button from "./components/MyButton";
-
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [locations, setLocations] = useState([]);
+
+  // fetch('https://pinballmap.com/api/v1/machines.json')
+  //   .then(response => response.json())
+  //   .then(console.log("ALL MACHINES", json()))
+
+  const handleNearMeClick = async () => {
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      setLatitude(position.coords.latitude.toFixed(6));
+      setLongitude(position.coords.longitude.toFixed(6));
+    } catch (error) {
+      console.error('Error getting current location:', error);
+
+      if (error.code === error.PERMISSION_DENIED) {
+        //in case user denied the geolocation request
+        alert('Please allow the browser to access your location for this feature.')
+      }
+    }
+  };
+
+  const handleSearchClick = async () => {
+    try {
+      const response = await axios.get(`https://pinballmap.com/api/v1/locations/closest_by_lat_lon.json?lat=${latitude}&lon=${longitude}`);
+
+
+      console.log("RESPONSE", response)
+      const data = response.data;
+
+      // console.log(data);
+
+
+      setLocations(data);
+    } catch (error) {
+      console.error('Error fetchinig pinball locations:', error);
+    }
+  }
   return (
     <div className="App">
-      hello world
-      <Button text={"Near me"} />
+      <label>
+        Latitude:
+        <input type="text" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+      </label>
+      <label>
+        Longitude:
+        <input type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+      </label>
+      <Button onClick={handleNearMeClick} text={"Near me"} />
+      <Button onClick={handleSearchClick} text={"Search"} />
+
+      <div>
+        <h2>Pinball Locations</h2>
+        <ul>
+          {locations.map((location) => (
+            <li key={location.id}>{location.name}</li>
+
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
