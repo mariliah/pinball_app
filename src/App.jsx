fef910fbb5bdfc2ca2449088663b2c3376c3f1b0
Marilia <ramonap2013@gmail.com>
2023-11-27 10:53:04 -0600

    start over

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,20 @@
-import Button from "./components/MyButton";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+export default function App() {
 
-function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-  const [locations, setLocations] = useState([]);
-
-  //information about machines
-
-  // const allMachines = fetch('https://pinballmap.com/api/v1/machines.json')
-  //   .then(response => response.json())
-  //   .then(json => console.log("ALL MACHINES", json))
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    })
-
-    console.log("LATITUDE IS:", latitude)
-    console.log("LONGITUDE IS:", longitude)
-  }, [latitude, longitude])
-
-  const handleNearMeClick = async () => {
-    try {
-      const position = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      });
-
-      setLatitude(position.coords.latitude.toFixed(6));
-      setLongitude(position.coords.longitude.toFixed(6));
-    } catch (error) {
-      console.error('Error getting current location:', error);
-
-      if (error.code === error.PERMISSION_DENIED) {
-        //in case user denied the geolocation request
-        alert('Please allow the browser to access your location for this feature.')
-      }
-    }
-  };
-
-  const handleSearchClick = async () => {
-    try {
-      const response = await axios.get(`https://pinballmap.com/api/v1/locations/closest_by_lat_lon.json?lat=${latitude}&lon=${longitude}`);
-
-
-      console.log("RESPONSE", response)
-      const data = response.data;
-
-      // console.log(data);
-
-
-      setLocations(data);
-    } catch (error) {
-      console.error('Error fetchinig pinball locations:', error);
-    }
-  }
   return (
     <div className="App">
-      <label onSubmit={handleSubmit}>
-        Latitude:
-        <input type="text" value={latitude} onChange={(e) => setLatitude(e.target.value)} />
+      <label>
+        <input type="text" />
       </label>
       <label>
-        Longitude:
-        <input type="text" value={longitude} onChange={(e) => setLongitude(e.target.value)} />
+        <input type="text" />
       </label>
-      <Button onClick={handleNearMeClick} text={"Near me"} />
-      <Button onClick={handleSearchClick} text={"Search"} />
+      <br />
+      <button>Near me</button>
+      <button>Search</button>
 
       <div>
-        <h2>Pinball Locations</h2>
-        <ul>
-          {locations.map((location) => (
-            <li key={location.id}>{location.name}</li>
-          ))}
-        </ul>
+        <h2>Pinball locations</h2>
       </div>
     </div>
-  );
+  )
 }
-
-export default App;
